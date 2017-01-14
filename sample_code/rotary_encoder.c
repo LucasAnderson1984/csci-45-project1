d361c73542cd8c93a4ae3e948585d4b4d2507cc4
Lucas Anderson <landerson_khs@hotmaill.com>
2017-01-13 22:49:46 -0800

    Use zip rotary_encoder.c file

--- sample_code/rotary_encoder.c
+++ sample_code/rotary_encoder.c
@@ -4,65 +4,68 @@
 #include <stdlib.h>
 #include <wiringPi.h>
 
-#define RoAPin 0
-#define RoBPin 1
-#define SWPin 2
+#define  RoAPin    0
+#define  RoBPin    1
+#define  SWPin     2
 
-static volatile int globalCounter = 0;
+static volatile int globalCounter = 0 ;
+
 unsigned char flag;
 unsigned char Last_RoB_Status;
 unsigned char Current_RoB_Status;
 
-void btnISR(void) {
-  globalCounter = 0;
+void btnISR(void)
+{
+	globalCounter = 0;
 }
 
-void rotaryDeal(void) {
-  Last_RoB_Status = digitalRead(RoBPin);
+void rotaryDeal(void)
+{
+	Last_RoB_Status = digitalRead(RoBPin);
 
-  while(!digitalRead(RoAPin)) {
-    Current_RoB_Status = digitalRead(RoBPin);
-    flag = 1;
-  }
+	while(!digitalRead(RoAPin)){
+		Current_RoB_Status = digitalRead(RoBPin);
+		flag = 1;
+	}
 
-  if(flag == 1) {
-    flag = 0;
-
-    if((Last_RoB_Status == 0) && (Current_RoB_Status == 1))
-      globalCounter++;
-
-    if((Last_RoB_Status == 1) && (Current_RoB_Status == 0))
-      globalCounter--;
-  }
+	if(flag == 1){
+		flag = 0;
+		if((Last_RoB_Status == 0)&&(Current_RoB_Status == 1)){
+			globalCounter ++;
+		}
+		if((Last_RoB_Status == 1)&&(Current_RoB_Status == 0)){
+			globalCounter --;
+		}
+	}
 }
 
 int main(void)
 {
-  if(wiringPiSetup() < 0) {
-    fprintf(stderr, "Unable to setup wiringPi:%s\n", strerror(errno));
-    return 1;
-  }
+	if(wiringPiSetup() < 0){
+		fprintf(stderr, "Unable to setup wiringPi:%s\n",strerror(errno));
+		return 1;
+	}
 
-  pinMode(SWPin, INPUT);
-  pinMode(RoAPin, INPUT);
-  pinMode(RoBPin, INPUT);
-  pullUpDnControl(SWPin, PUD_UP);
+	pinMode(SWPin, INPUT);
+	pinMode(RoAPin, INPUT);
+	pinMode(RoBPin, INPUT);
 
-  if(wiringPiISR(SWPin, INT_EDGE_FALLING, &btnISR) < 0) {
-    fprintf(stderr, "Unable to init ISR\n", strerror(errno));
-    return 1;
-  }
+	pullUpDnControl(SWPin, PUD_UP);
 
-  int tmp = 0;
+    if(wiringPiISR(SWPin, INT_EDGE_FALLING, &btnISR) < 0){
+		fprintf(stderr, "Unable to init ISR\n",strerror(errno));
+		return 1;
+	}
 
-  while(1) {
-    rotaryDeal();
+	int tmp = 0;
 
-    if (tmp != globalCounter) {
-      printf("%d\n", globalCounter);
-      tmp = globalCounter;
-    }
-  }
+	while(1){
+		rotaryDeal();
+		if (tmp != globalCounter){
+			printf("%d\n", globalCounter);
+			tmp = globalCounter;
+		}
+	}
 
-  return 0;
+	return 0;
 }
