3f78bf79a4829d0bef0a6c212ba302957ad1542b
Lucas Anderson <landerson_khs@hotmaill.com>
2017-01-15 19:25:03 -0800

    Update readme.md file with project specs Update sample code for c

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# csci-45-project1
+# csci-45-project1 #
+## Team ##
+Joses Paras
+Lucas Anderson
+Tanpreet Dhaliwal
+
+## Rules ##
+The specific rules for this project are:
+1. You must use at least two sensors (either checked out from me, or bought on your own nickel).
+2. It must be controlled by software on the Pi
+3. Use at least 2 LEDs to show the status of the thing
+4. Bonus points for awesomeness
+
+- - - -
+# Project Proposal #
+Create a puzzle box using the sensors we checked out in class.
+_Fill out more later.  Have the team discussion on the specifics._
+
+Example:
+[Xbox Puzzle](https://www.youtube.com/watch?v=o_8e6XgAiow)
+
+## Sensors ##
+Rotary Encoder
+Touch Switch
+Photoresistor
+
+## Responsibilities ##
+Programming the touch switch - Joses
+Programming the rotary encoder - Lucas
+Programming the photoresistor - Tanpreet
--- sample_code/rotary_encoder.c
+++ sample_code/rotary_encoder.c
@@ -4,44 +4,41 @@
 #include <stdlib.h>
 #include <wiringPi.h>
 
-#define  RoAPin    0
-#define  RoBPin    1
-#define  SWPin     2
+#define  RoAPin 0
+#define  RoBPin 1
+#define  SWPin  2
 
-static volatile int globalCounter = 0 ;
+static volatile int globalCounter = 0;
 
 unsigned char flag;
 unsigned char Last_RoB_Status;
 unsigned char Current_RoB_Status;
 
-void btnISR(void)
-{
+void btnISR(void) {
 	globalCounter = 0;
 }
 
-void rotaryDeal(void)
-{
+void rotaryDeal(void) {
 	Last_RoB_Status = digitalRead(RoBPin);
 
-	while(!digitalRead(RoAPin)){
+	while(!digitalRead(RoAPin)) {
 		Current_RoB_Status = digitalRead(RoBPin);
 		flag = 1;
 	}
 
-	if(flag == 1){
+	if(flag == 1) {
 		flag = 0;
-		if((Last_RoB_Status == 0)&&(Current_RoB_Status == 1)){
-			globalCounter ++;
-		}
-		if((Last_RoB_Status == 1)&&(Current_RoB_Status == 0)){
-			globalCounter --;
-		}
+
+		if((Last_RoB_Status == 0) && (Current_RoB_Status == 1))
+			globalCounter++;
+
+		if((Last_RoB_Status == 1) && (Current_RoB_Status == 0))
+			globalCounter--;
 	}
 }
 
-int main(void)
-{
-	if(wiringPiSetup() < 0){
+int main(void) {
+	if(wiringPiSetup() < 0) {
 		fprintf(stderr, "Unable to setup wiringPi:%s\n",strerror(errno));
 		return 1;
 	}
@@ -52,16 +49,17 @@ int main(void)
 
 	pullUpDnControl(SWPin, PUD_UP);
 
-    if(wiringPiISR(SWPin, INT_EDGE_FALLING, &btnISR) < 0){
+  if(wiringPiISR(SWPin, INT_EDGE_FALLING, &btnISR) < 0) {
 		fprintf(stderr, "Unable to init ISR\n",strerror(errno));
 		return 1;
 	}
 
 	int tmp = 0;
 
-	while(1){
+	while(1) {
 		rotaryDeal();
-		if (tmp != globalCounter){
+
+		if (tmp != globalCounter) {
 			printf("%d\n", globalCounter);
 			tmp = globalCounter;
 		}
